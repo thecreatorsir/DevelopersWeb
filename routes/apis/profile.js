2f1c340aa5a2bc089b4e7bf6c0cdb6ca3d3299f0
Shubham Sharma <s.psharma887@gmail.com>
2021-04-05 17:11:03 +0530

    profile protected route

--- routes/apis/profile.js
+++ routes/apis/profile.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 //   @route api/profile/test
 //   @desc test route for profile
@@ -9,4 +14,21 @@ router.get('/test',(req,res)  => res.json({
   msg : "profile route",
 }))
 
+//   @route api/profile
+//   @desc current user profile
+//   @access private 
+
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res) => {
+  const errors = {};
+  Profile.findOne({user:req.user.id})
+  .then(profile => {
+      if(!profile){
+        errors.noprofile = 'No profile available for the current user';
+        return res.status(404).json(errors);
+      }else{
+        return res.json(profile);
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
 module.exports = router;
