1497267af36548f917673c28765ed5328f0e0520
Shubham Sharma <s.psharma887@gmail.com>
2021-04-10 11:51:58 +0530

    register and login form added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,68 @@
 import React, { Component } from "react";
 
 export class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const User = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    alert(JSON.stringify(User));
+  }
   render() {
-    return <div>Login</div>;
+    return (
+      <div className='login'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-8 m-auto'>
+              <h1 className='display-4 text-center'>Log In</h1>
+              <p className='lead text-center'>
+                Sign in to your DeveloperWeb account
+              </p>
+              <form onSubmit={(event) => this.handleSubmit(event)}>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    className='form-control form-control-lg'
+                    placeholder='Email Address'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className='form-control form-control-lg'
+                    placeholder='Password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type='submit' className='btn btn-info btn-block mt-4' />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
