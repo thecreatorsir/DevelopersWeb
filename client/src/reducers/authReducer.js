beb09d5aa64e2f9d3d5703497a7140f582b468d2
Shubham Sharma <s.psharma887@gmail.com>
2021-04-12 11:16:06 +0530

    adding redux store

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialstate = {
+  isAuthenticated: false,
+  user: {},
+};
+
+const authReducer = (state = initialstate, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default authReducer;
