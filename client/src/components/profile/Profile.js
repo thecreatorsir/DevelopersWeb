1051cbc9aaaa3ccdb833fd2927e82b1fe614550e
Shubham Sharma <s.psharma887@gmail.com>
2021-04-19 18:25:58 +0530

    profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -14,15 +14,44 @@ export class Profile extends Component {
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className='row'>
+            <div className='col-md-6'>
+              <Link to='/profiles' className='btn btn-light mb-3 float-left'>
+                Back To Profiles
+              </Link>
+            </div>
+            <div className='col-md-6' />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCred />
+          <ProfileGithub />
+        </div>
+      );
+    }
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCred />
-        <ProfileGithub />
+      <div className='profile'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(null, { getProfileByHandle })(Profile);
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfileByHandle })(Profile);
