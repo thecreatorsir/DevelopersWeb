9c5d4d3e8e270cc862b8331d60d112b50501f6de
Shubham Sharma <s.psharma887@gmail.com>
2021-04-21 15:39:47 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const users = require('./routes/apis/users');
-const profile = require('./routes/apis/profile');
-const posts = require('./routes/apis/posts');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/apis/users");
+const profile = require("./routes/apis/profile");
+const posts = require("./routes/apis/posts");
+const passport = require("passport");
+const path = require("path");
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//DB  config 
-const db = require('./config/keys').mongoURI;
+//DB  config
+const db = require("./config/keys").mongoURI;
 
 //Connect to mongoDB
 
 mongoose
-     .connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
-     .then(()=>console.log('mongoDB Connected'))
-     .catch((err)=>console.log(err));
-        
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongoDB Connected"))
+  .catch((err) => console.log(err));
 
 //passport middleware
 app.use(passport.initialize());
 
 // passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //use routes
 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
+//sever static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set the static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`server is running on port ${port}`);
-})
+});
